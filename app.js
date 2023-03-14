823e3d0088e6da9491159df55c0335cf23584e10
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-03-14 16:19:45 +0530

    updated js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const res = document.querySelector('.res')
 const originalString = str.textContent
 makeHtml(originalString)
 
+ 
 input.addEventListener('keydown',(e) => {
     const ptr = input.value
     if(ptr.length===0) return
@@ -17,27 +18,20 @@ input.addEventListener('keydown',(e) => {
 })
 
 input.addEventListener('input', () => {
-    
     const p = input.value;
-    if(p.length===0) return
-    if(originalString[p.length-1]===p[p.length-1]){
-        const index = document.querySelector(`p.given-text span.span${p.length-1}`)
-        index.classList.add('right')
 
+    if(p.length===0 || p.length===originalString.length+1) return
+    const index = document.querySelector(`p.given-text span.span${p.length-1}`)
+
+    if(originalString[p.length-1]===p[p.length-1]){
+        index.classList.add('right')
     }
     else{
-        const index = document.querySelector(`p.given-text span.span${p.length-1}`)
         index.classList.add('wrong')
         index.innerText=p[p.length-1]
     }
 })
 
-function print(){
-    const message = document.createElement('p');
-    message.innerText = 'correct'
-    message.classList.add('.mess');
-    res.appendChild(message);
-}
 
 function makeHtml(originalString){
     str.innerHTML=''
@@ -47,5 +41,4 @@ function makeHtml(originalString){
         span.classList.add(`span${i}`)
         str.insertAdjacentElement('beforeend',span)
     }
-    console.log(str)
 }
