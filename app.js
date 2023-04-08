d90594a51d07ab4145efa417691d1a6468d5a824
Amanjot-001 <amanjotsingh2309@gmail.com>
2023-04-08 22:13:28 +0530

    fixed backspace and spaces issue and refactored

--- app.js
+++ app.js
@@ -14,94 +14,137 @@ let grossWords = 0;
 
 //creating span in each char of given text
 makeHtml(originalString);
-function makeHtml(originalString){
-    str.innerHTML=''
-    for(let i=0; i<originalString.length; i++){
+function makeHtml(originalString) {
+    str.innerHTML = ''
+    for (let i = 0; i < originalString.length; i++) {
         const span = document.createElement('span')
         span.textContent = originalString[i];
         span.classList.add(`span${i}`)
-        str.insertAdjacentElement('beforeend',span)
+        str.insertAdjacentElement('beforeend', span)
     }
 }
 
 //counting total words present in given text
 const totalWords = tWords();
-function tWords (){
+function tWords() {
     let words = 0;
     let char_flag = false;
     let space_flag = false;
-    for(let ch of originalString){
-        if(ch === ' ')
+    for (let ch of originalString) {
+        if (ch === ' ')
             space_flag = true;
         else
             char_flag = true;
-        
-        if(char_flag === true && space_flag === true){
+
+        if (char_flag === true && space_flag === true) {
             words++;
             char_flag = false;
             space_flag = false;
-        } 
+        }
     }
-    return words+1;
+    return words + 1;
 }
-console.log(totalWords)
+// console.log(totalWords)
 
 // selecting timer start value
 let count = parseInt(dropdown.value);
-dropdown.addEventListener('change', ()=> {
+dropdown.addEventListener('change', () => {
     count = parseInt(dropdown.value);
 })
 
 function timer() {
-  if (count === 0 || over === true) {
-    clock.textContent = count;
-    input.disabled = true;
-    score();
-  } else {
-    clock.textContent = count;
-    count--;
-    setTimeout(timer, 1000);
-  }
+    if (count === 0 || over === true) {
+        clock.textContent = count;
+        input.disabled = true;
+        score();
+    } else {
+        clock.textContent = count;
+        count--;
+        setTimeout(timer, 1000);
+    }
 }
 
-function score(){
-    res.textContent = (correctWords/originalString.length)*100;
+function score() {
+    res.textContent = (correctWords / originalString.length) * 100;
 }
 
-input.addEventListener('keydown',(e) => {
-    const ptr = input.value;
-    if(ptr.length===0) return;
-    if(e.key==='Backspace'){
-        const index = document.querySelector(`p.given-text span.span${ptr.length-1}`);
-        index.classList.remove('right')
-        index.classList.remove('wrong')
-        index.innerText=originalString[ptr.length-1]
+input.addEventListener('keydown', (e) => {
+    let ptr = input.value;
+    let flag = false; // to check if removing notTyped class
+    if (e.key === 'Backspace') {
+        let index = document.querySelector(`p.given-text span.span${ptr.length - 1}`);
+        // remove notTyped class from all chars till the place where space was entered
+        while (index.classList.contains('notTyped')) {
+            index.classList.remove('notTyped')
+            index.innerText = originalString[ptr.length - 1]
+            ptr = ptr.slice(0, -1);
+            // console.log(ptr);
+            index = document.querySelector(`p.given-text span.span${ptr.length - 1}`);
+            flag = true;
+        }
+        if (flag) { // only if above loop ran
+            // index = document.querySelector(`p.given-text span.span${ptr.length}`);
+            input.value = ptr;
+            input.value += originalString[ptr.length - 1]
+        }
+        else { // remove classes
+            index.classList.remove('right')
+            index.classList.remove('wrong')
+            index.innerText = originalString[ptr.length - 1]
+        }
     }
 })
 
-input.addEventListener('input', () => {
-    const p = input.value;
-    if(!startTimer){
+let p = '';
+
+input.addEventListener('input', (e) => {
+    // p += input.value[input.value.length-1];
+    p = input.value;
+    if (p[0] == ' ') { // to prevent typing space in start
+        p = '';
+        input.value = '';
+        return;
+    }
+    if (p[p.length - 1] == ' ' && p[p.length - 2] == ' ') { // to prevent typing consecutive spaces
+        input.value = input.value.slice(0, -1)
+        return;
+    }
+    if (!startTimer) {
         setTimeout(timer, 1000);
         startTimer = true;
     }
-    const index = document.querySelector(`p.given-text span.span${p.length-1}`);
+    let index = document.querySelector(`p.given-text span.span${p.length - 1}`);
 
-    if(originalString[p.length-1]===p[p.length-1]){
-        if(p.length !== 0)
+    if (originalString[p.length - 1] === p[p.length - 1]) {
+        if (p.length !== 0)
             index.classList.add('right');
-        if(visited[p.length-1]===0) correctWords++;
+        if (visited[p.length - 1] === 0) correctWords++;
     }
-    else{
+    else {
+        // if space is typed b/w word
+        if (p[p.length - 1] == ' ') {
+            input.value = input.value.slice(0, -1);
+            // add notTyped to all till next word, and copy orginal text
+            for (let i = p.length - 1; i < originalString.length; i++) {
+                if (originalString[p.length - 1] == ' ') break;
+                index.classList.add('notTyped');
+                p += originalString[i];
+                input.value += originalString[i];
+                index = document.querySelector(`p.given-text span.span${p.length - 1}`);
+            }
+            input.value += " ";
+            index.classList.add('notTyped');
+            return;
+        }
         index.classList.add('wrong')
-        index.innerText=p[p.length-1]
+        index.innerText = p[p.length - 1]
     }
 
-    if(p === originalString || count===0){
+    if (p === originalString || count === 0) {
         input.disabled = true;
         over = true;
         score();
         return;
     }
-    visited[p.length-1]=1;
+    visited[p.length - 1] = 1;
 })
