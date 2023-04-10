4d560f64fa30167b831dc569403323b4271ab9aa
Yash Poonia <93772907+yash7xm@users.noreply.github.com>
2023-04-10 12:24:02 +0530

    caret added

--- app.js
+++ app.js
@@ -12,27 +12,31 @@ let startTimer = false; // to start timer when user started typing
 let correctWords = 0;
 let grossWords = 0;
 
-// when click on text, user can start typing
-str.addEventListener('click', () => {
-    input.focus();   
-})
-// input.style.opacity = 0;
-// input.style.height = '0';
-// input.style.width = '0';
-// input.style.border = '0';
-// input.style.padding = '0';
-
 //creating span in each char of given text
 makeHtml(originalString);
 function makeHtml(originalString) {
     str.innerHTML = ''
-    for (let i = 0; i < originalString.length; i++) {
+    const span = document.createElement('span')
+    span.textContent = `|${originalString[0]}`;
+    span.classList.add(`span${0}`)
+    str.insertAdjacentElement('beforeend', span)
+    for (let i = 1; i < originalString.length; i++) {
         const span = document.createElement('span')
         span.textContent = originalString[i];
         span.classList.add(`span${i}`)
         str.insertAdjacentElement('beforeend', span)
     }
 }
+
+// hiding the input field
+str.addEventListener('click', () => {
+    input.focus();
+});
+
+input.style.height = '0';
+input.style.width = '0';
+input.style.border = '0';
+input.style.padding = '0';
 
 //counting total words present in given text
 const totalWords = tWords();
@@ -54,7 +58,6 @@ function tWords() {
     }
     return words + 1;
 }
-// console.log(totalWords)
 
 // selecting timer start value
 let count = parseInt(dropdown.value);
@@ -78,9 +81,11 @@ function score() {
     res.textContent = (correctWords / originalString.length) * 100;
 }
 
+//backspace stuff
 input.addEventListener('keydown', (e) => {
     let ptr = input.value;
     let flag = false; // to check if removing notTyped class
+    if (ptr.length < 1) return; //if no character is left
     if (e.key === 'Backspace') {
         let index = document.querySelector(`p.given-text span.span${ptr.length - 1}`);
         // remove notTyped class from all chars till the place where space was entered
@@ -88,12 +93,11 @@ input.addEventListener('keydown', (e) => {
             index.classList.remove('notTyped')
             index.innerText = originalString[ptr.length - 1]
             ptr = ptr.slice(0, -1);
-            // console.log(ptr);
             index = document.querySelector(`p.given-text span.span${ptr.length - 1}`);
             flag = true;
         }
         if (flag) { // only if above loop ran
-            // index = document.querySelector(`p.given-text span.span${ptr.length}`);
+            // index = document.querySelector(`p.given-text span.span${ptr.length-1}`);
             input.value = ptr;
             input.value += originalString[ptr.length - 1]
         }
@@ -105,11 +109,17 @@ input.addEventListener('keydown', (e) => {
     }
 })
 
-let p = '';
-
+//handling the input
 input.addEventListener('input', (e) => {
-    // p += input.value[input.value.length-1];
-    p = input.value;
+    let p = input.value;
+    console.log(p);
+    if (p.length < 1) {
+        let index = document.querySelector(`p.given-text span.span${0}`);
+        index.innerText = `|${originalString[0]}`;
+        return;
+    }
+    let index = document.querySelector(`p.given-text span.span${p.length - 1}`);
+
     if (p[0] == ' ') { // to prevent typing space in start
         p = '';
         input.value = '';
@@ -119,39 +129,68 @@ input.addEventListener('input', (e) => {
         input.value = input.value.slice(0, -1)
         return;
     }
-    if(originalString[p.length-1] == ' ' && p[p.length-1] != ' '){ // we can't type extra chars in a word than present in original
-        input.value = input.value.slice(0,-1);
-        return;
-    }
 
     if (!startTimer) {
         setTimeout(timer, 1000);
         startTimer = true;
     }
-    let index = document.querySelector(`p.given-text span.span${p.length - 1}`);
 
+    // if correct word is typed
     if (originalString[p.length - 1] === p[p.length - 1]) {
-        if (p.length !== 0)
+        if (p.length !== 0) {
+            if (p.length > 1) {
+                let beforeSpan = document.querySelector(`p.given-text span.span${p.length - 2}`);
+                index.textContent = `${index.textContent}|`;
+                beforeSpan.textContent = `${beforeSpan.textContent[0]}`;
+            }
+            else {
+                if (index.textContent.length > 1) index.textContent = `${index.textContent[1]}|`;
+                else {
+                    index.textContent = `${index.textContent[0]}|`;
+                }
+            }
             index.classList.add('right');
+        }
         if (visited[p.length - 1] === 0) correctWords++;
     }
     else {
         // if space is typed b/w word
         if (p[p.length - 1] == ' ') {
             input.value = input.value.slice(0, -1);
+            let oneTime = true;
             // add notTyped to all till next word, and copy orginal text
             for (let i = p.length - 1; i < originalString.length; i++) {
                 if (originalString[p.length - 1] == ' ') break;
+                if (oneTime) {
+                    let beforeSpan = document.querySelector(`p.given-text span.span${p.length - 2}`);
+                    beforeSpan.textContent = `${beforeSpan.textContent[0]}`;
+                }
+                oneTime = false;
                 index.classList.add('notTyped');
                 p += originalString[i];
                 input.value += originalString[i];
                 index = document.querySelector(`p.given-text span.span${p.length - 1}`);
             }
+            index.textContent = `${index.textContent}|`
             input.value += " ";
             index.classList.add('notTyped');
             return;
         }
-        index.classList.add('wrong')
+        if (originalString[p.length - 1] == ' ' && p[p.length - 1] != ' ') {
+            console.log(input.value);
+            input.value = input.value.slice(0, -1);
+            return;
+        }
+        if (p.length > 1) {
+            let beforeSpan = document.querySelector(`p.given-text span.span${p.length - 2}`);
+            index.textContent = `${index.textContent}|`;
+            beforeSpan.textContent = `${beforeSpan.textContent[0]}`;
+        }
+        else {
+            if (index.textContent.length > 1) index.textContent = `${index.textContent[1]}|`;
+            else index.textContent = `${index.textContent[0]}|`;
+        }
+        index.classList.add('wrong');
         // index.innerText = p[p.length - 1]
     }
 
